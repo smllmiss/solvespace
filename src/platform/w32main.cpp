3ebaaef455d3520a928c485c681bb353ea6d321e
GU3MS <37662048+GU3MS@users.noreply.github.com>
2018-04-17 00:44:50 -0400

    Update w32main.cpp

--- src/platform/w32main.cpp
+++ src/platform/w32main.cpp
@@ -1553,16 +1553,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     // And now it's the message loop. All calls in to the rest of the code
     // will be from the wndprocs.
 	
-	Message(_("Wall Panel Layout, Assembly and Inventory Management.\n"
+	Message(_("Wall Panel Layout, Assembly and Inventory Management\n"
 "\n"
 "\n"
-" ___________        ___________        ___________    \n"
-"[           ]      [           ]      [           ]   \n"
-"[           ]      [           ]      [           ]   \n"
-"[    NEW    ]      [   OPEN    ]      [ INVENTORY ]   \n"
-"[           ]      [           ]      [           ]   \n"
-"[           ]      [           ]      [           ]   \n"
-"[___________]      [___________]      [___________]   \n"
+" ____________        ____________        ____________    \n"
+"|            |      |            |      |            |   \n"
+"|            |      |            |      |            |   \n"
+"|    NEW     |      |    OPEN    |      |  INVENTORY |   \n"
+"|            |      |            |      |            |   \n"
+"|            |      |            |      |            |   \n"
+"|____________|      |____________|      |____________|   \n"
+"\n"		  
 "Developed w/open-source code http://solvespace.com/\n"
 "\n"
 "© 2008-2016 Jonathan Westhues and other authors.\n"));
