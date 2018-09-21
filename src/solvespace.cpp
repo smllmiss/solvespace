149dd87f45b9c76e966928c9e25f50a749f44ca1
GU3MS <37662048+GU3MS@users.noreply.github.com>
2018-09-21 08:41:39 -0400

    Update solvespace.cpp

--- src/solvespace.cpp
+++ src/solvespace.cpp
@@ -475,7 +475,7 @@ void SolveSpaceUI::MenuFile(Command id) {
             // If the user is exporting something where it would be
             // inappropriate to include the constraints, then warn.
             if(!SS.GW.showConstraints &&
-                (exportFile.HasExtension("txt") ||
+                !(exportFile.HasExtension("txt") ||
                  fabs(SS.exportOffset) > LENGTH_EPS))
             {
                 Message(_("Constraints are currently shown, and will be exported "
