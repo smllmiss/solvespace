c8c4003019f7e8c6fe72d1a5d9c7ef8bce52a352
GU3MS <37662048+GU3MS@users.noreply.github.com>
2018-10-09 09:50:11 -0400

    Update panelize.cpp

--- src/panelize.cpp
+++ src/panelize.cpp
@@ -4,10 +4,10 @@
 // Author: Akash Kothari  <akothar3@ncsu.edu>            //
 //-------------------------------------------------------//
 
-// #include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/opencv.hpp>
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/imgproc/imgproc.hpp>
+//#include <opencv2/opencv.hpp>
 #include <iostream>
 #include <vector>
 #include <cstdint>
