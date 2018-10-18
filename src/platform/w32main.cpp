b600ab84373363636f36e4017a65512448fe52c6
GU3MS <37662048+GU3MS@users.noreply.github.com>
2018-10-17 21:28:13 -0400

    Update w32main.cpp

--- src/platform/w32main.cpp
+++ src/platform/w32main.cpp
@@ -5,6 +5,7 @@
 //
 // Copyright 2008-2013 Jonathan Westhues.
 //-----------------------------------------------------------------------------
+#include <process.h>
 #include <time.h>
 
 #include "config.h"
