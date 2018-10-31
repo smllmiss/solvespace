e3d505571d8d32afe2ebf0a34befa393b27cca49
GU3MS <37662048+GU3MS@users.noreply.github.com>
2018-10-31 17:19:55 -0400

    Update constraint.cpp

--- src/constraint.cpp
+++ src/constraint.cpp
@@ -125,7 +125,9 @@ void Constraint::MenuConstrain(Command id) {
     auto const &gs = SS.GW.gs;
 
     switch(id) {
-//        case Command::DISTANCE_DIA:
+        case Command::DISTANCE_DIA: {
+            SplashBox("\n	   		    Wall Panel Layout, Assembly and Inventory Management\n\n\n\n\nElizabeth Anderson, Taylor Ascher, Akash Kothari, Guthrie Shriver\n	   		Sponsored by Doggett Concrete™\n\n\n\n				Solvespace (http://solvespace.com) is an open-source software\n	  		    Jonathon Westhues and other authors © 2008-2016 .", 15, 67);
+            }
         case Command::REF_DISTANCE: {
             if(gs.points == 2 && gs.n == 2) {
                 c.type = Type::PT_PT_DISTANCE;
